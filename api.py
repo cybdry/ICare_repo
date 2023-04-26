645c32dd29706e491d2890c92795a9793eb93cdb
cabdala68 <aragasanmo@gmail.com>
2023-04-27 00:56:07 +0300

    fix bugs

--- api.py
+++ api.py
@@ -14,64 +14,74 @@ import whisper
 from googletrans import Translator
 
 
-openai.api_key = os.getenv("E_OPENAI_KEY")
+openai.api_key = os.getenv("A_OPENAI_KEY")
 
 message_from_box1 = " "
 open_ai_output = " "
 
 translator = Translator()
 
+
 def chatgpt_api(input_text):
     messages = [
-        {"role":"system","content":" "}
+        {"role": "system", "content": " "}
     ]
     if input_text:
         messages.append({
-            "role":"user",
-            "content":input_text
+            "role": "user",
+            "content": input_text
         })
         chat_completion = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",messages = messages
+            model="gpt-3.5-turbo", messages=messages
         )
-        reply  = chat_completion.choices[0].messages.content
+        reply = chat_completion.choices[0].messages.content
         return reply
     else:
         raise ValueError("Input must not be empty")
 
+
 def get_api_wrappper(audio):
     print("get_api_wrappper functions is called")
     resut_text, audio_file = convert_audio_to_text(audio)
-    key,*get_user = user_requirement(resut_text)
+    key, *get_user = user_requirement(resut_text)
+    print("Get user", get_user)
+    global open_ai_output
 
     if key:
-        openai_output = chatgpt_api(get_user[0])
+        open_ai_output = chatgpt_api(get_user[0])
+        translated = translator.translate(open_ai_output, src="en", dest="es")
+        audio_file_spanish = convert_text_to_audio(
+            translated, "es", "openai_output.mp3")
 
-    if len(openai_output) != 0:
-        translated = translator.translate(openai_output,src="en",dest="es")
-        audio_file_spanish = convert_text_to_audio(translated,"es","openai_output.mp3")
     else:
-        translated = translator.translate(get_user[1],src="en",dest="es")
-        audio_file_spanish = convert_text_to_audio(translated,"es","openai_output.mp3")
-        
-    return [translated,openai_output,audio_file_spanish]
+        print("English: ", get_user[0])
+        translated = translator.translate(
+            get_user[1], src="en", dest="es").text
+        print("Translated text: ", translated)
 
-def gradio_interface(): 
-    output_1 = gr.Textbox(label = "Speech To Text(English)") 
-    output_2 = gr.Textbox(label = "Api output")
+        audio_file_spanish = convert_text_to_audio(
+            translated, "es", "openai_output.mp3")
+
+    return [translated, open_ai_output, audio_file_spanish]
+
+
+def gradio_interface():
+    output_1 = gr.Textbox(label="Speech To Text(English)")
+    output_2 = gr.Textbox(label="Api output")
     output_3 = gr.Audio(audio_file)
     inputs = ["mic"]
 
     gradio_launcher = gr.Interface(
-        title = "ICare Web Interface",
-        fn = get_api_wrappper, # Put the final functions here
-        inputs = inputs,
-        outputs = [
-            output_1,output_2,output_3
+        title="ICare Web Interface",
+        fn=get_api_wrappper,  # Put the final functions here
+        inputs=gr.inputs.Audio(source="microphone", type="filepath"),
+        outputs=[
+            output_1, output_2, output_3
         ],
-        live = True)
+        live=True)
 
     gradio_launcher.launch()
 
+
 if __name__ == "__main__":
     gradio_interface()
-
--- voice_utils.py
+++ voice_utils.py
@@ -9,6 +9,7 @@ model = whisper.load_model("base")
 audio_file = "openai_output.mp3"
 language = "es"
 
+
 def convert_audio_to_text(audio_voice):
     print("convert_audio_to_text called")
 
@@ -16,29 +17,20 @@ def convert_audio_to_text(audio_voice):
     audio = whisper.pad_or_trim(audio)
 
     mel = whisper.log_mel_spectrogram(audio).to(model.device)
-    _,probs = model.detect_language(mel)
+    _, probs = model.detect_language(mel)
 
-    options = whisper.DecodingOptions()
-    result = whisper.decode(model,mel,options)
+    options = whisper.DecodingOptions(fp16=False)
+    result = whisper.decode(model, mel, options)
     result_text = result.text
 
-    audio_obj = gTTS(text = result_text,
-                     lang = language,
-                     slow = False)
+    audio_obj = gTTS(text=result_text,
+                     lang=language)
     audio_obj.save(audio_file)
-    return result_text,audio_file
+    return result_text, audio_file
 
 
-def convert_text_to_audio(text_t,language_t,audio_file):
-    audio_obj  = gTTS(text = text_t,
-                      lang = language_t,
-                      slow = False)
+def convert_text_to_audio(text_t, language_t, audio_file):
+    audio_obj = gTTS(text=text_t,
+                     lang=language_t)
     audio_obj.save(audio_file)
     return audio_file
-
-
-
-
-
-
-
